896a082bdd0c7b4f59fd1495d6b002800b025ad1
Saroj-Thapa199 <sarojthapa199@gmail.com>
2024-07-15 13:36:35 +0545

    Update App.jsx

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -9,7 +9,7 @@ import ListAlbum from './pages/ListAlbum';
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 
-export const url = 'https://spotify-backend-k81v.onrender.com/'
+export const url = 'https://spotify-backend-k81v.onrender.com'
 
 const App = () => {
   return (
